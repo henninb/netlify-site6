1bfa0e48fb56a36ea4ba4adbadf4f358ef0c6b08
Brian Henning <henninb@msn.com>
2022-09-10 07:12:52 -0500

    wip

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-echo npx create-next-app example-nextjs
-echo 'http://localhost:3000/hello'
-echo 'http://localhost:3000/cars'
-#npm install
-touch .env.local
-npm install
-npm run dev
-
+netlify deploy --prod
 
 exit 0
